cb3ea3f477b95db2cfea794344dada03588bc794
Edward Valdivia Jr <evvaldiv@gmail.com>
2020-06-05 16:33:37 -0500

    homework-mods

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,25 @@
-const rollButton = document.querySelector("roll-button")
-const diceRolls = []
-let total = document.querySelector("total")
-const allRolls = document.querySelector("show-all-rolls")
-const list = document.querySelector("ol")
+const rollButton = document.querySelector(".roll-button")
 
+const diceRolls = []
+const total = document.querySelector(".total")
+const allRolls = document.querySelector(".show-all-rolls")
+const list = document.querySelector("ol")
+console.log(rollButton)
+console.log(total)
 rollButton.addEventListener("click", function () {
     let diceRolled = document.querySelector("#number-of-dice").value;
     let counter = 0
-    let total = 0
+    let sum = 0
     while (counter < diceRolled) {
-        let diceRolled = Math.floor(Math.random() * 6 + 1);
-        console.assert.log(diceRoll)
-        diceRolled.push(diceRolled)
-        total += 1
+        let diceRoll = Math.floor(Math.random() * 6 + 1);
+        console.log(diceRoll)
+        diceRolls.push(diceRoll)
+        sum += diceRoll
         counter += 1
     }
-    console.log(diceRolled)
-    total.innerHtml = "Sum of All Rolls" + total
+    console.log(diceRolls)
+    total.innerHTML = "Sum of All Rolls" + sum
+    console.log(total)
 })
 
 allRolls.addEventListener("click", function () {
